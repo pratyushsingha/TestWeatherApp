784d5359da2fcd89b270e6abbf74d82a1b291815
pratyushsingha <pratyushsingha83@gmail.com>
2023-04-22 20:37:15 +0530

    hidding the api key

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Set the output directory for the built files
+    outDir: 'build',
+    // Generate source maps for the built files
+    sourcemap: true,
+    // Specify any environment variables you want to use in the built files
+    define: {
+      'process.env': process.env,
+    },
+  },
+});
